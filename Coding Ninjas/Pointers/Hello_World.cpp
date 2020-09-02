1d649a0eb3716ae8ea16800132eb4f118132924d
Vijay <you@example.com>
2020-09-02 12:35:59 +0530

    Testing things out

---
+++ Coding Ninjas/Pointers/Hello_World.cpp
@@ -0,0 +1,39 @@
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std; 
+
+
+
+void solve() 
+{ 
+	cout<<"HELLO WORLD!"<<endl;
+
+
+
+} 
+
+
+
+int main() 
+{ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  
+#ifndef ONLINE_JUDGE     
+freopen("error.txt", "w", stderr); 
+#endif 
+  
+    int t = 1; 
+    /*is Single Test case?*/ cin >> t; 
+    while (t--) { 
+        solve(); 
+         
+    } 
+  
+    cerr << "Total Time Taken : " << (float)clock() / CLOCKS_PER_SEC << " Seconds!" << endl; 
+    return 0; 
+} 
+
+ 
