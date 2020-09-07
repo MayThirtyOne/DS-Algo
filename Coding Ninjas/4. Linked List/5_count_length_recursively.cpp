cd70fb7cce9d6fd7d6765c7007df5c3e9cb3aa5f
Vijay <may31.py@gmail.com>
2020-09-07 19:23:29 +0530

    Regular Harmless Commit

---
+++ Coding Ninjas/4. Linked List/5_count_length_recursively.cpp
@@ -0,0 +1,82 @@
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std; 
+
+
+class Node{
+public:
+	int data;
+	Node *next;
+	Node(int data){
+		this->data=data;
+		next = NULL;
+	}
+};
+
+int countll(Node *head){
+	if(head == NULL) return 0;
+	Node *temp=head;
+	return 1 + countll(temp->next);
+}
+
+
+Node* takeIP(){
+	int data;
+	cin>>data;
+	Node* head = NULL;
+	Node* tail = NULL;
+	while(data!=-1){
+		Node *n = new Node(data);
+		if(head == NULL){
+			head = n;
+			tail = n;
+		}
+		else{
+			tail->next = n;
+			tail=tail->next;
+
+		}
+		cin>>data;
+
+	}
+	return head;
+}
+
+void solve() 
+{ 
+
+	Node* head = takeIP();
+	cout<<countll(head)<<endl;
+
+
+
+
+
+
+} 
+
+
+
+int main() 
+{ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  
+#ifndef ONLINE_JUDGE     
+freopen("error.txt", "w", stderr); 
+#endif 
+  
+    int t = 1; 
+    /*is Single Test case?*/ cin >> t; 
+    while (t--) { 
+        solve(); 
+         
+    } 
+  
+    cerr << "Total Time Taken : " << (float)clock() / CLOCKS_PER_SEC << " Seconds!" << endl; 
+    return 0; 
+} 
+
+ 
