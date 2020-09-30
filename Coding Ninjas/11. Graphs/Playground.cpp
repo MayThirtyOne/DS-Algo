81178a7da2cc380540cb6d1867456c1ca874ef9a
Vijay <may31.py@gmail.com>
2020-09-30 20:57:48 +0530

    Regular Harmless Commit

---
+++ Coding Ninjas/11. Graphs/Playground.cpp
@@ -0,0 +1,44 @@
+	 
+	#include <bits/stdc++.h>
+	#define ll long long int
+
+	using namespace std; 
+
+
+
+	void solve() 
+	{ 
+
+	string s;
+	cin>>s;
+	int sum=0;
+	for(int i=0;i<s.length();i++){
+		int index=((int)s[i])%32 -1;
+		sum+=27-index;
+	}
+	cout<<sum<<endl;
+
+	} 
+
+
+
+	int main() 
+	{ 
+	    ios_base::sync_with_stdio(false); 
+	    cin.tie(NULL); 
+	  
+	#ifndef ONLINE_JUDGE     
+	freopen("error.txt", "w", stderr); 
+	#endif 
+	  
+	    int t = 1; 
+	   while (t--) { 
+	        solve(); 
+	         
+	    } 
+	  
+	    cerr << "Total Time Taken : " << (float)clock() / CLOCKS_PER_SEC << " Seconds!" << endl; 
+	    return 0; 
+	} 
+
+	 
