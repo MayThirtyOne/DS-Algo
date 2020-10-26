f1e57022d6bbfaedfaf02174f37d3bf8a9deceb6
Vijay <may31.py@gmail.com>
2020-10-26 19:49:57 +0530

    Regular Harmless Commit

---
+++ IPK/11.cpp
@@ -0,0 +1,43 @@
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std; 
+
+
+
+void solve() 
+{ 
+    char str1[100000+10];
+    char str2[100000+10];
+    cin>>str1>>str2;
+
+    cout<<str1+2<<str2<<endl;
+
+
+
+
+} 
+
+
+
+int main() 
+{ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  
+#ifndef ONLINE_JUDGE     
+freopen("error.txt", "w", stderr); 
+#endif 
+  
+    int t = 1; 
+    while (t--) { 
+        solve(); 
+         
+    } 
+  
+    cerr << "Total Time Taken : " << (float)clock() / CLOCKS_PER_SEC << " Seconds!" << endl; 
+    return 0; 
+} 
+
+ 
